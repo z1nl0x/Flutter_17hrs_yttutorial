5c7986541624916cca5b4589af6b4ab8a889da4f
z1nl0x <paulo.kreft@gmail.com>
2025-02-02 12:38:00 -0300

    List of packages installed

--- instacrap_clone/lib/main.dart
+++ instacrap_clone/lib/main.dart
@@ -1,53 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
- 
+
 void main() {
-  Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  runApp(
+    App(),
   );
-  runApp(const MyApp());
 }
- 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
- 
-  // This widget is the root of your application.
+
+class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Instacrap',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Flutter App',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      //   useMaterial3: true,
+      // ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
-      home: StartingPage(title: 'Instacrap - Clone'),
+      debugShowCheckedModeBanner: false,
+      home: const StartPage(),
     );
   }
 }
- 
-class StartingPage extends StatelessWidget {
-  final String title;
-  const StartingPage({required this.title, super.key});
- 
- 
+
+
+class StartPage extends StatelessWidget {
+const StartPage({ Key? key }) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: Text(title),
+        title: const Text('Instacrap - Clone'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World!',
-            ),
-          ],
+        child: Text(
+          'Helo World!'
         ),
       ),
     );
