78942efc64603465dca4a5e39c05b162110ec3aa
z1nl0x <paulo.kreft@gmail.com>
2025-02-02 16:26:26 -0300

    Project updates

--- instacrap_clone/lib/main.dart
+++ instacrap_clone/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:instacrap_clone/state/auth/backend/authenticator.dart';
 import 'firebase_options.dart';
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,14 +29,10 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Instacrap',
       darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blueGrey,
-        indicatorColor: Colors.blueGrey
-      ),
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue
-      ),
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blueGrey,
+          indicatorColor: Colors.blueGrey),
+      theme: ThemeData(brightness: Brightness.dark, primarySwatch: Colors.blue),
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       home: const StartPage(),
@@ -37,21 +40,25 @@ class App extends StatelessWidget {
   }
 }
 
-
 class StartPage extends StatelessWidget {
-const StartPage({ Key? key }) : super(key: key);
+  const StartPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Instacrap - Clone'),
-      ),
-      body: Center(
-        child: Text(
-          'Helo World!'
+        appBar: AppBar(
+          title: const Text('Instacrap - Clone'),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            TextButton(
+              onPressed: () async {
+                final result = Authenticator().loginWithGoogle();
+                print(result);
+              },
+              child: const Text('Logar!'),
+            )
+          ],
+        ));
   }
 }
